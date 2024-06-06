Temporary fix to keep code running. s03 is now in Variance.

docs/examples_tutorial/e00_intro_set/s06_tone_stft_atom_styx_TO_FIX.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
     stft_power = 2 * np.abs(stft_complex) ** 2
 
     # Compute complex wavelet transform (cwt) from signal duration using the Gabor atoms
-    cwt_complex, _, time_cwt_s, frequency_cwt_hz = atoms.cwt_chirp_from_sig(
+    cwt_complex, _, time_cwt_s, frequency_cwt_hz = atoms_TO_REPLACE.cwt_chirp_from_sig(
         sig_wf=mic_sig,
         frequency_sample_rate_hz=frequency_sample_rate_hz,
         band_order_Nth=order_number_input,

docs/examples_tutorial/e00_intro_set/s09_n-wave_tdr.py
@@ -9,18 +9,18 @@ from quantum_inferno.synth import synthetics_TO_FIX
 import scipy.signal as signal
 
 
-
 print(__doc__)
-EVENT_NAME = 'nwave_tfr'
-station_id_str = 'synth'
+EVENT_NAME = "nwave_tfr"
+station_id_str = "synth"
 # alpha: Shape parameter of the Tukey window, representing the fraction of the window inside the cosine tapered region.
 # If zero, the Tukey window is equivalent to a rectangular window.
 # If one, the Tukey window is equivalent to a Hann window.
 alpha = 1
 
 
-def n_wave_period_center(intro_s: float, n_wave_duration_s: float, outro_s: float, sample_rate_hz: float) -> \
-        [np.ndarray, np.ndarray, np.ndarray]:
+def n_wave_period_center(
+    intro_s: float, n_wave_duration_s: float, outro_s: float, sample_rate_hz: float
+) -> [np.ndarray, np.ndarray, np.ndarray]:
     """
     N-Wave is simple
 
@@ -33,13 +33,13 @@ def n_wave_period_center(intro_s: float, n_wave_duration_s: float, outro_s: floa
 
     total_duration_s = intro_s + n_wave_duration_s + outro_s
     time_points = int(sample_rate_hz * total_duration_s)
-    time_s = np.arange(time_points)/sample_rate_hz
-    tau = (time_s - intro_s)/n_wave_duration_s
+    time_s = np.arange(time_points) / sample_rate_hz
+    tau = (time_s - intro_s) / n_wave_duration_s
     # Initialize
     p_n_wave = np.zeros(time_points)
     # Initialize time ranges
     sigint1 = np.where((intro_s <= time_s) & (time_s <= time_s[-1] - outro_s))  # nwave
-    p_n_wave[sigint1] = (1. - 2*(time_s[sigint1]-intro_s)/n_wave_duration_s)
+    p_n_wave[sigint1] = 1.0 - 2 * (time_s[sigint1] - intro_s) / n_wave_duration_s
 
     return p_n_wave, time_s, tau
 
@@ -51,42 +51,48 @@ if __name__ == "__main__":
 
     order_number_input = 6
     EVENT_NAME = "N-Wave Test"
-    station_id_str = 'Synth'
+    station_id_str = "Synth"
 
-    frequency_sample_rate_hz = 8000.
+    frequency_sample_rate_hz = 8000.0
     pseudo_period_main_s = 0.2
 
     # Duration set by the number of cycles
-    mult = 8.
+    mult = 8.0
     decimate = 10
-    intro = mult*pseudo_period_main_s
-    sig_wf_raw, sig_time_s_raw, sig_tau = n_wave_period_center(intro_s=intro,
-                                                               n_wave_duration_s=pseudo_period_main_s,
-                                                               outro_s=mult*pseudo_period_main_s,
-                                                               sample_rate_hz=frequency_sample_rate_hz)
-    sig_wf_aa = synthetics.antialias_half_nyquist(synth=np.copy(sig_wf_raw))
+    intro = mult * pseudo_period_main_s
+    sig_wf_raw, sig_time_s_raw, sig_tau = n_wave_period_center(
+        intro_s=intro,
+        n_wave_duration_s=pseudo_period_main_s,
+        outro_s=mult * pseudo_period_main_s,
+        sample_rate_hz=frequency_sample_rate_hz,
+    )
+    sig_wf_aa = synthetics_TO_FIX.antialias_half_nyquist(synth=np.copy(sig_wf_raw))
     sig_wf_dec = signal.decimate(np.copy(sig_wf_aa), decimate)
     sig_time_s = sig_time_s_raw[::10]
 
-    sig_wf_lp00 = styx_fft.butter_lowpass(sig_wf=sig_wf_dec,
-                                          frequency_sample_rate_hz=frequency_sample_rate_hz/decimate,
-                                          frequency_cut_high_hz=320.,
-                                          filter_order=4,
-                                          tukey_alpha=0)
+    sig_wf_lp00 = styx_fft.butter_lowpass(
+        sig_wf=sig_wf_dec,
+        frequency_sample_rate_hz=frequency_sample_rate_hz / decimate,
+        frequency_cut_high_hz=320.0,
+        filter_order=4,
+        tukey_alpha=0,
+    )
 
-    sig_wf_bp1 = styx_fft.butter_bandpass(sig_wf=sig_wf_dec,
-                                          frequency_sample_rate_hz=frequency_sample_rate_hz/decimate,
-                                          frequency_cut_low_hz=20,
-                                          frequency_cut_high_hz=320,
-                                          filter_order=4,
-                                          tukey_alpha=0.)
+    sig_wf_bp1 = styx_fft.butter_bandpass(
+        sig_wf=sig_wf_dec,
+        frequency_sample_rate_hz=frequency_sample_rate_hz / decimate,
+        frequency_cut_low_hz=20,
+        frequency_cut_high_hz=320,
+        filter_order=4,
+        tukey_alpha=0.0,
+    )
 
-    plt.plot(sig_time_s_raw-intro, sig_wf_raw, label='Nominal 0.2s N wave @ 8 kHz')
-    plt.plot(sig_time_s-intro, sig_wf_lp00, '--', label='Lowpassed, fc=320 Hz')
-    plt.plot(sig_time_s-intro, sig_wf_bp1, label='Bandpassed, fc=20-320 Hz')
-    plt.title('Nominal, lowpassed, and bandpassed filtered N wave')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Normalized')
+    plt.plot(sig_time_s_raw - intro, sig_wf_raw, label="Nominal 0.2s N wave @ 8 kHz")
+    plt.plot(sig_time_s - intro, sig_wf_lp00, "--", label="Lowpassed, fc=320 Hz")
+    plt.plot(sig_time_s - intro, sig_wf_bp1, label="Bandpassed, fc=20-320 Hz")
+    plt.title("Nominal, lowpassed, and bandpassed filtered N wave")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Normalized")
     plt.xlim(-0.2, 0.4)
     plt.legend()
     plt.tight_layout()