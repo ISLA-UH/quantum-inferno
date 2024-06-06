deee1a542b70b516e9062388227b72a25d744396
Samuel Kei Takazawa <tokyo_k@me.com>
2024-06-06 12:47:33 -1000

    s05 is now in Variance.

--- docs/examples_tutorial/e00_intro_set/s05_tone_stft_quantum.py
+++ docs/examples_tutorial/e00_intro_set/s05_tone_stft_quantum.py
@@ -41,8 +41,10 @@ if __name__ == "__main__":
     ] = benchmark_signals.well_tempered_tone(frequency_center_hz=frequency_tone_hz, add_noise_taper_aa=True)
 
     # Computed and nominal values
-    mic_sig_rms = np.std(mic_sig)
-    mic_sig_rms_nominal = 1 / np.sqrt(2)
+    # mic_sig_rms = np.std(mic_sig)
+    # mic_sig_rms_nominal = 1 / np.sqrt(2)
+    mic_sig_var = np.var(mic_sig)
+    mic_sig_var_nominal = 1 / 2
 
     # Compute the Welch PSD; averaged spectrum over sliding windows
     frequency_welch_hz, psd_welch_power = signal.welch(
@@ -91,9 +93,9 @@ if __name__ == "__main__":
 
     stft_power = 2 * np.abs(stft_complex) ** 2
 
-    fft_rms_welch = np.sqrt(np.abs(psd_welch_power)) / mic_sig_rms
-    fft_rms_spect = np.sqrt(np.average(psd_spec_power, axis=1)) / mic_sig_rms
-    fft_rms_stft = np.sqrt(np.average(stft_power, axis=1)) / mic_sig_rms
+    fft_rms_welch = np.abs(psd_welch_power) / mic_sig_var
+    fft_rms_spect = np.average(psd_spec_power, axis=1) / mic_sig_var
+    fft_rms_stft = np.average(stft_power, axis=1) / mic_sig_var
 
     # Express in bits; revisit
     # TODO: What units shall we use? Evaluate Stockwell first
@@ -126,9 +128,9 @@ if __name__ == "__main__":
     ax2.semilogx(frequency_welch_hz, fft_rms_welch)
     ax2.semilogx(frequency_spect_hz, fft_rms_spect)
     ax2.semilogx(frequency_stft_hz, fft_rms_stft, ".-")
-    ax2.set_title("Welch and Spect FFT (RMS), f = " + str(round(frequency_center_fft_hz * 100) / 100) + " Hz")
+    ax2.set_title("Welch and Spect FFT (VAR), f = " + str(round(frequency_center_fft_hz, 2)) + " Hz")
     ax2.set_xlabel("Frequency, hz")
-    ax2.set_ylabel("FFT RMS * sqrt(2)")
+    ax2.set_ylabel("FFT VAR * sqrt(2)")
     ax2.grid(True)
 
     # Plot the inverse stft (full recovery)
