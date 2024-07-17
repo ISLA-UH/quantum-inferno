c380356b5c168566cf18a645b5561e96fe5064a7
Samuel Kei Takazawa <tokyo_k@me.com>
2024-07-16 14:28:34 -1000

    Added spectrogram method and option to change padding

--- quantum_inferno/utilities/short_time_fft.py
+++ quantum_inferno/utilities/short_time_fft.py
@@ -9,8 +9,8 @@ from typing import Tuple, Literal
 
 from quantum_inferno.utilities.calculations import round_value
 
-SCALING_OPTIONS = ["magnitude", "psd", None]
 scaling_type = Literal["magnitude", "psd", None]
+padding_type = Literal["zeros", "edge", "even", "odd"]
 
 # TODO: ADD THE SPECTROGRAM FUNCTIONALITY
 
@@ -46,8 +46,8 @@ def get_stft_object_tukey(
         print(f"Warning: Tukey alpha {tukey_alpha} must be between 0 and 1, using 0.25 as the default value")
         tukey_alpha = 0.25
 
-    if scaling not in SCALING_OPTIONS:
-        print(f"Warning: scaling {scaling} must be one of {SCALING_OPTIONS}, using 'magnitude' as the default value")
+    if scaling not in scaling_type:
+        print(f"Warning: scaling {scaling} must be one of {scaling_type}, using 'magnitude' as the default value")
         scaling = "magnitude"
 
     # calculate the values to be used in the ShortTimeFFT object
@@ -70,6 +70,7 @@ def stft_tukey(
     segment_length: int,
     overlap_length: int,
     scaling: scaling_type = "magnitude",
+    padding: padding_type = "zeros",
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     Calculate the Short-Time Fourier Transform (STFT) of a signal with a Tukey window using ShortTimeFFT class
@@ -81,14 +82,19 @@ def stft_tukey(
     :param segment_length: length of the segment
     :param overlap_length: length of the overlap
     :param scaling: scaling of the STFT (default is None, other options are 'magnitude' and 'psd)
+    :param padding: padding method for the STFT (default is 'zeros', other options are 'edge', 'even', and 'odd')
     :return: time, frequency, and magnitude of the STFT
     """
+    # check if padding is valid
+    if padding not in padding_type:
+        print(f"Warning: padding {padding} must be one of {padding_type}, using 'zeros' as the default value")
+        padding = "zeros"
 
     # create the ShortTimeFFT object
     stft_obj = get_stft_object_tukey(sample_rate_hz, tukey_alpha, segment_length, overlap_length, scaling)
 
     # calculate the STFT with detrending
-    stft_magnitude = stft_obj.stft_detrend(x=timeseries, detr="constant")
+    stft_magnitude = stft_obj.stft_detrend(x=timeseries, detr="constant", padding=padding)
 
     # calculate the time and frequency bins
     time_bins = np.arange(start=0, stop=stft_obj.delta_t * np.shape(stft_magnitude)[1], step=stft_obj.delta_t)
@@ -127,3 +133,44 @@ def istft_tukey(
     timestamps = np.arange(start=0, stop=last_window_index / sample_rate_hz, step=1 / sample_rate_hz)
 
     return timestamps, stft_obj.istft(stft_to_invert, k1=last_window_index)
+
+
+# get the spectrogram with default parameters
+def spectrogram_tukey(
+    timeseries: np.ndarray,
+    sample_rate_hz: float or int,
+    tukey_alpha: float,
+    segment_length: int,
+    overlap_length: int,
+    scaling: scaling_type = "magnitude",
+    padding: padding_type = "zeros",
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Calculate the Spectrogram of a signal with a Tukey window using ShortTimeFFT class
+    Returns the time, frequency, and spectrogram similar to legacy scipy.signal.spectrogram
+
+    :param timeseries: input signal
+    :param sample_rate_hz: sample rate of the signal
+    :param tukey_alpha: shape parameter of the Tukey window
+    :param segment_length: length of the segment
+    :param overlap_length: length of the overlap
+    :param scaling: scaling of the spectrogram (default is None, other options are 'magnitude' and 'psd)
+    :param padding: padding of the signal (default is 'zeros', other options are 'periodic' and 'constant')
+    :return: time, frequency, and magnitude of the STFT
+    """
+    # check if padding is valid
+    if padding not in padding_type:
+        print(f"Warning: padding {padding} must be one of {padding_type}, using 'zeros' as the default value")
+        padding = "zeros"
+
+    # Make the ShortTimeFFT object
+    stft_obj = get_stft_object_tukey(sample_rate_hz, tukey_alpha, segment_length, overlap_length, scaling)
+
+    # Calculate the spectrogram
+    spectrogram = stft_obj.spectrogram(x=timeseries, padding=padding)
+
+    # calculate the time and frequency bins
+    time_bins = np.arange(start=0, stop=stft_obj.delta_t * np.shape(spectrogram)[1], step=stft_obj.delta_t)
+    frequency_bins = stft_obj.f
+
+    return frequency_bins, time_bins, spectrogram
