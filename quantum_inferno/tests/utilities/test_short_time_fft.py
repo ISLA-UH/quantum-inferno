945bbbeedef3c87a57f5a7f891e50988888bcf03
Samuel Kei Takazawa <tokyo_k@me.com>
2024-07-17 14:42:43 -1000

    updating tests for short_time_fft

--- quantum_inferno/tests/utilities/test_short_time_fft.py
+++ quantum_inferno/tests/utilities/test_short_time_fft.py
@@ -23,3 +23,58 @@ class TestPicker(unittest.TestCase):
             scaling="magnitude",
         )
         self.assertIsInstance(stft_obj, ShortTimeFFT)
+        self.assertTrue(stft_obj.invertible)
+        self.assertEqual(stft_obj.scaling, "magnitude")
+        self.assertEqual(stft_obj.fft_mode, "onesided")
+        self.assertEqual(stft_obj.m_num, self.fft_nd)  # check window length
+        self.assertEqual(stft_obj.mfft, self.fft_nd)  # check window length with mfft
+        self.assertEqual(stft_obj.hop, self.fft_nd // 2)  # check hop length
+        self.assertEqual(stft_obj.delta_f, self.resolution)
+
+
+    def test_stft_tukey(self):
+        frequencies, times, stfts = short_time_fft.stft_tukey(
+            timeseries=self.signal,
+            sample_rate_hz=self.sample_rate,
+            tukey_alpha=self.tukey_alpha,
+            segment_length=self.fft_nd,
+            overlap_length=self.fft_nd // 2,
+            scaling="magnitude",
+        )
+        self.assertEqual(times.shape[0], len(self.timestamps) // (self.fft_nd // 2) + 1)
+        self.assertEqual(frequencies.shape, (self.fft_nd // 2 + 1,))
+        self.assertEqual(stfts.shape, (self.fft_nd // 2 + 1, len(self.timestamps) // (self.fft_nd // 2) + 1))
+
+    def test_istft_tukey(self):
+        frequencies, times, stfts = short_time_fft.stft_tukey(
+            timeseries=self.signal,
+            sample_rate_hz=self.sample_rate,
+            tukey_alpha=self.tukey_alpha,
+            segment_length=self.fft_nd,
+            overlap_length=self.fft_nd // 2,
+            scaling="magnitude",
+        )
+        reconstructed_time, reconstructed_signal = short_time_fft.istft_tukey(
+            stft_to_invert=stfts,
+            sample_rate_hz=self.sample_rate,
+            tukey_alpha=self.tukey_alpha,
+            segment_length=self.fft_nd,
+            overlap_length=self.fft_nd // 2,
+            scaling="magnitude",
+        )
+        self.assertEqual(len(reconstructed_signal), len(self.signal))
+        self.assertTrue(np.allclose(self.signal, reconstructed_signal, atol=1e-15))
+        self.assertTrue(np.allclose(self.timestamps, reconstructed_time, atol=1e-15))
+
+    def test_spectrogram_tukey(self):
+        frequencies, times, spectrogram = short_time_fft.spectrogram_tukey(
+            timeseries=self.signal,
+            sample_rate_hz=self.sample_rate,
+            tukey_alpha=self.tukey_alpha,
+            segment_length=self.fft_nd,
+            overlap_length=self.fft_nd // 2,
+            scaling="magnitude",
+        )
+        self.assertEqual(times.shape[0], len(self.timestamps) // (self.fft_nd // 2) + 1)
+        self.assertEqual(frequencies.shape, (self.fft_nd // 2 + 1,))
+        self.assertEqual(spectrogram.shape, (self.fft_nd // 2 + 1, len(self.timestamps) // (self.fft_nd // 2) + 1))
